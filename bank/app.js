1f7fa80db1e59981e431f7d624c4ea523abc7550
ScientifcW <nbrichard@bupt.edu.cn>
2023-11-28 10:01:43 +0800

    12

---
+++ bank/app.js
@@ -0,0 +1,35 @@
+const routes = {
+    '/login': { templateId: 'login' },
+    '/dashboard': { templateId: 'dashboard' },
+  };
+  
+  function updateRoute() {
+    const path = window.location.pathname;
+    const route = routes[path];
+  
+    const template = document.getElementById(route.templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    app.innerHTML = '';
+    app.appendChild(view);
+  }
+
+  function navigate(path) {
+    const path = window.location.pathname;
+    const route = routes[path];
+
+    if (!route) {
+        return navigate('/login');
+      }
+
+    window.history.pushState({}, path, path);
+    updateRoute();
+  }
+
+  function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+  }
+
+window.onpopstate = () => updateRoute();
+updateRoute();
