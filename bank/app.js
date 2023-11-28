f503e7dea93724d08c48bf8e6dfc8b167f7ec15f
ScientifcW <nbrichard@bupt.edu.cn>
2023-11-28 12:08:15 +0800

    09

--- bank/app.js
+++ bank/app.js
@@ -1,35 +1,35 @@
-const routes = {
-    '/login': { templateId: 'login' },
-    '/dashboard': { templateId: 'dashboard' },
-  };
-  
-  function updateRoute() {
-    const path = window.location.pathname;
-    const route = routes[path];
-  
-    const template = document.getElementById(route.templateId);
-    const view = template.content.cloneNode(true);
-    const app = document.getElementById('app');
-    app.innerHTML = '';
-    app.appendChild(view);
+function updateRoute(templateId) {
+  const template = document.getElementById(templateId);
+  const view = template.content.cloneNode(true);
+  const app = document.getElementById('app');
+  app.innerHTML = '';
+  app.appendChild(view);
+}
+updateRoute('login');
+
+async function createAccount(account) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: account
+    });
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
+async function register() {
+  const registerForm = document.getElementById('registerForm');
+  const formData = new FormData(registerForm);
+  const jsonData = JSON.stringify(Object.fromEntries(formData));
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    return console.log('An error occured:', result.error);
   }
 
-  function navigate(path) {
-    const path = window.location.pathname;
-    const route = routes[path];
+  console.log('Account created!', result);
+}
 
-    if (!route) {
-        return navigate('/login');
-      }
-
-    window.history.pushState({}, path, path);
-    updateRoute();
-  }
-
-  function onLinkClick(event) {
-    event.preventDefault();
-    navigate(event.target.href);
-  }
-
-window.onpopstate = () => updateRoute();
-updateRoute();
